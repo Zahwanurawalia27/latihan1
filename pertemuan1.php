d90c8810fbe3269e51ab566a2c412c5bd471dbf8
Zahwanurawalia27 <znurawalia@gmail.com>
2022-09-21 17:26:06 +0700

    first commit

---
+++ pertemuan1.php
@@ -0,0 +1,12 @@
+<?php
+$data = 4;
+$data2 = 2;
+echo $data/$data2;          //menampilkan data dalam variabel
+print_r("awikwok");
+var_dump("hahaha");
+var_dump(1.2);              //menampilkan data angka
+var_dump(true);             //menampilkan boolean
+
+
+
+?>
---
+++ proses.php
@@ -0,0 +1,9 @@
+<?php
+
+$siswa = $_POST['input1'];
+
+echo $siswa;
+
+?>
+
+
